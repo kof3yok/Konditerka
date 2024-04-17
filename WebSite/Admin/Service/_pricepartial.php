61e4a8af01d5d8742cd4dd8e0a12f86561e188bf
kof3yok <71557295+kof3yok@users.noreply.github.com>
2024-04-18 02:01:16 +0300

    Update _pricepartial.php

--- WebSite/Admin/Service/_pricepartial.php
+++ WebSite/Admin/Service/_pricepartial.php
@@ -1,3 +1,11 @@
+// Этот код является частью веб-приложения на PHP и представляет собой обработчик POST-запросов, связанных с операциями над ценами товаров.
+// Проверяется метод запроса (POST) и наличие определенных ключей в массиве $_POST.
+// Далее подключается файл с классом DatabaseConnector, который устанавливает соединение с базой данных, и файл с классом productprice, который, скорее всего, содержит логику работы с ценами продуктов.
+// Создается экземпляр класса ProductPrice с переданным объектом базы данных.
+// В зависимости от параметров в $_POST объект ProductPrice инициализируется различными данными, такими как ID, ProductID, Name, Description, Price.
+// Затем, в зависимости от параметров в $_POST, вызываются различные методы этого объекта: Update(), Create(), Passive(), Active(). Вероятно, методы Update(), Create(), Passive(), Active() выполняют соответственно обновление, создание, отключение и активацию цен продуктов.
+// В конце каждого блока происходит перенаправление пользователя на страницу productedit.php с передачей параметра id равного ProductID.
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['save_update'])) {
 
@@ -47,9 +55,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['save_update'])) {
     }
     header('Location: ../productedit.php?id=' . $pp->ProductID);
 }
+// В коде есть еще одна функция GetPrice($id), которая используется для получения информации о цене продукта по его ID.
 function GetPrice($id)
 {
-    // Get user input
     require_once '../src/System/DatabaseConnector.php';
     require_once '../Service/productprice.php';
     $database = new Database();
