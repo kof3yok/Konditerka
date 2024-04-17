6a8a193faacfcfd4b29dff5cbba737f28f278af6
kof3yok <71557295+kof3yok@users.noreply.github.com>
2024-04-18 00:58:48 +0300

    Update hitproduct.php

--- WebSite/Service/hitproduct.php
+++ WebSite/Service/hitproduct.php
@@ -1,3 +1,4 @@
+// Этот код на PHP определяет класс HitProduct, который представляет объект для работы с базой данных для таблицы hitproduct
 <?php
 require_once 'base.php';
 class HitProduct
@@ -9,12 +10,16 @@ class HitProduct
     public $ID;
     public $ProductID;
     public $CreationDate;
-
+// Конструктор класса, который принимает объект базы данных $db и инициализирует соответствующее свойство $conn
     public function __construct($db)
     {
         $this->conn = $db;
     }
-
+    // Выполняет вставку новой записи в таблицу "hitproduct" с указанными данными.    
+    // Генерирует уникальный идентификатор (ID) с помощью функции GUID().
+    // Записывает дату создания (CreationDate) в текущий момент времени.
+    // Выполняет запрос с использованием подготовленного выражения и передает данные через массив.
+    // Возвращает true, если запрос выполнен успешно, иначе false.
     function Create()
     {
         $query = "INSERT INTO " . $this->table_name . "(ID,
@@ -31,6 +36,9 @@ class HitProduct
         if ($q->execute($data)) return true;
         return false;
     }
+    // Удаляет запись из таблицы "hitproduct" по указанному ID.
+    // Использует подготовленное выражение для безопасной передачи данных в запрос.
+    // Возвращает true, если удаление выполнено успешно, иначе false.
     function Delete()
     {
         $query = "DELETE FROM " . $this->table_name .
@@ -42,6 +50,8 @@ class HitProduct
         return false;
         //return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+    // Выполняет выборку данных из различных таблиц базы данных, чтобы получить полную информацию о продуктах.
+    // Возвращает результат выполнения запроса в виде объекта PDOStatement, который содержит выбранные строки.
     function GetAll()
     {
         $query = "SELECT p.ID,
