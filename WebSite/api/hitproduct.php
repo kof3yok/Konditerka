4c19869dc206abc3253e15070489a3450a750739
kof3yok <71557295+kof3yok@users.noreply.github.com>
2024-04-21 23:15:38 +0300

    Update hitproduct.php

--- WebSite/api/hitproduct.php
+++ WebSite/api/hitproduct.php
@@ -1,17 +1,22 @@
+// API для взаимодействия с товарами между приложением и БД
 <?php
+// Установка заголовков CORS (Cross-Origin Resource Sharing): Этот блок кода устанавливает необходимые заголовки для поддержки CORS, что позволяет выполнение запросов из разных источников.
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+// Подключение необходимых файлов и классов: Код подключает файлы DatabaseConnector.php, hitproduct.php и sendJson.php, которые, вероятно, содержат определения классов для работы с базой данных и обработки запросов.
 include_once '../src/System/DatabaseConnector.php';
 include_once '../Service/hitproduct.php';
 include_once '../Service/sendJson.php';
-
+// Обработка запросов: Код определяет метод HTTP-запроса и в зависимости от этого метода и параметра method в запросе выполняет соответствующие действия:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $method = $_GET['method'];
     switch ($method) {
+// Создание записи: Если метод запроса - POST и параметр method равен 'create', создается новая запись с использованием данных из тела запроса. 
+// Если все необходимые поля заполнены, создается новая запись в базе данных и возвращается ответ со статусом 200 и данными о созданной записи. 
+// В противном случае возвращается сообщение об ошибке и статус 422 (непроцессируемая сущность).
         case 'create':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -39,6 +44,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// Удаление записи: Если метод запроса - POST и параметр method равен 'delete', удаляется запись с идентификатором, переданным в теле запроса. 
+// Если удаление прошло успешно, возвращается ответ со статусом 200 и сообщением об успешном удалении. В противном случае возвращается сообщение об ошибке и статус 200.
         case 'delete':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -64,6 +71,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// Получение всех записей: Если метод запроса - POST и параметр method равен 'getall', получаются все записи из базы данных и возвращаются в формате JSON. 
+// Если записи успешно получены, возвращается ответ со статусом 200 и данными о записях. В противном случае возвращается сообщение об ошибке и статус 200.
         case 'getall':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -101,6 +110,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     sendJson(200, 'Unexcepted Error');
                 }
             } catch (\Throwable $th) {
+            // Обработка исключений: В случае возникновения исключения во время выполнения кода, возвращается сообщение об ошибке и статус 500 (внутренняя ошибка сервера).
                 sendJson(500, 'Internal Server Error');
             }
             break;
