1508548a9db39c0ab1d014956788cacbcb3a4019
kof3yok <71557295+kof3yok@users.noreply.github.com>
2024-04-22 00:23:30 +0300

    Update userorderdetail.php

--- WebSite/api/userorderdetail.php
+++ WebSite/api/userorderdetail.php
@@ -1,10 +1,13 @@
+// API для взаимодействия с деталями историй заказов пользователя между приложением и БД
 <?php
+// Установка заголовков CORS: Устанавливаются заголовки для обеспечения кросс-доменных запросов (CORS). Это позволяет другим доменам отправлять запросы к этому серверу.
+// Включение поддержки методов HTTP: Устанавливаются заголовки, определяющие поддержку различных HTTP-методов (OPTIONS, GET, POST, PUT, DELETE).
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+// Подключение необходимых файлов и классов: Включаются файлы с определениями классов для работы с базой данных и сервисными функциями.
 include_once '../src/System/DatabaseConnector.php';
 include_once '../Service/userorderdetail.php';
 include_once '../Service/cart.php';
@@ -13,6 +16,8 @@ include_once '../Service/sendJson.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $method = $_GET['method'];
     switch ($method) {
+// Создание заказа: Если запрос имеет метод create, создается новый заказ на основе данных, полученных из тела запроса. 
+// Данные валидируются, затем создается экземпляр класса UserOrderDetail и вызывается метод Create(). Результат операции возвращается в виде JSON.
         case 'create':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -54,6 +59,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// Создание всех заказов: Если запрос имеет метод createall, создаются несколько заказов на основе данных, полученных из тела запроса. 
+// Данные валидируются, создается экземпляр класса UserOrderDetail, вызывается метод CreateAll(), а затем удаляются все товары из корзины. Результат операции возвращается в виде JSON.
         case 'createall':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -81,6 +88,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// Получение всех товаров по заказу: Если запрос имеет метод getall, возвращаются все товары, относящиеся к определенному заказу. 
+// Данные валидируются, создается экземпляр класса UserOrderDetail, вызывается метод GetAllByOrder(), данные форматируются и возвращаются в виде JSON.
         case 'getall':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -128,8 +137,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
             break;
         default:
+// Обработка ошибок: Если происходят ошибки в процессе выполнения запроса (например, неправильные данные, ошибка сервера и т. д.), отправляется соответствующий статус ошибки и сообщение об ошибке в виде JSON.
             sendJson(405, 'Invalid Request Method. HTTP method should be POST');
             break;
     }
 }
+//Завершение скрипта при некорректном методе запроса: Если метод запроса не POST, отправляется статус ошибки и сообщение об ошибке в виде JSON.
 sendJson(405, 'Invalid Request Method. HTTP method should be POST');
