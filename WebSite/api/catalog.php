e28c39f58d661f0ba284ee259182b91487292ca8
kof3yok <71557295+kof3yok@users.noreply.github.com>
2024-04-21 13:49:40 +0300

    Update catalog.php

--- WebSite/api/catalog.php
+++ WebSite/api/catalog.php
@@ -1,17 +1,21 @@
+// // API для взаимодействия с категориями товаров между приложением и БД
 <?php
+// Установка заголовков CORS: Устанавливает заголовки CORS (Cross-Origin Resource Sharing), чтобы разрешить доступ к ресурсам из разных источников. 
+// Это позволяет скриптам на одном домене взаимодействовать с ресурсами на другом домене без нарушения политики безопасности браузера.
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+// Подключение необходимых файлов и классов: Включает файлы DatabaseConnector.php, catalog.php и sendJson.php, которые, вероятно, содержат определения классов и функций, необходимых для работы с базой данных и отправки JSON-ответов.
 include_once '../src/System/DatabaseConnector.php';
 include_once '../Service/catalog.php';
 include_once '../Service/sendJson.php';
-
+// Обработка запросов методом POST: Если запрос был выполнен методом POST, код проверяет значение параметра method в URL-адресе запроса и в зависимости от этого выполняет одно из следующих действий: создание, удаление, обновление или получение всех записей каталога.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $method = $_GET['method'];
     switch ($method) {
+// create: Создает новый каталог. Проверяет наличие всех обязательных полей (в данном случае только name). Если поля не заполнены, возвращает ошибку 422. После этого создает каталог и возвращает его ID.
         case 'create':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -39,6 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// delete: Удаляет каталог по указанному ID. Проверяет наличие обязательного поля id. Если поле не указано, возвращает ошибку 422.
         case 'delete':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -64,6 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// update: Обновляет информацию о каталоге по указанному ID. Проверяет наличие обязательных полей id и name. Если поля не указаны, возвращает ошибку 422.
         case 'update':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -92,6 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 sendJson(500, 'Internal Server Error');
             }
             break;
+// getall: Получает все каталоги. Возвращает JSON-список всех каталогов товаров.
         case 'getall':
             try {
                 $data = json_decode(file_get_contents('php://input'));
@@ -123,9 +130,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
             break;
         default:
+// Обработка GET-запросов: В настоящем коде нет обработки GET-запросов, поэтому возвращается ошибка 405.
+// Отправка JSON-ответов: Функция sendJson используется для отправки JSON-ответов с соответствующим кодом состояния и данными.
             sendJson(405, 'Invalid Request Method. HTTP method should be POST');
             break;
     }
 } else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 }
+// Обработка ошибок: Если происходит исключение (например, ошибка в работе с базой данных), возвращается соответствующий код состояния и сообщение об ошибке.
 sendJson(405, 'Invalid Request Method. HTTP method should be POST');
