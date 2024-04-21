b13ab93da20d1c3139ad6cd9c6c40ad7d60b9216
kof3yok <71557295+kof3yok@users.noreply.github.com>
2024-04-22 01:11:31 +0300

    Update MyItemRecyclerViewAdapter.kt

--- app/src/main/java/com/example/cakeapp/MyItemRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/cakeapp/MyItemRecyclerViewAdapter.kt
@@ -218,11 +218,11 @@ class MyItemRecyclerViewAdapter(
                 alterDialog.setTitle("No Internet")
                 alterDialog.setMessage("Internet Connection can't be establish!")
                 alterDialog.setPositiveButton("Open Settings") { text, listener ->
-                    val settingsIntent = Intent(Settings.ACTION_SETTINGS)//open wifi settings
+                    val settingsIntent = Intent(Settings.ACTION_SETTINGS)
                 }
 
                 alterDialog.setNegativeButton("Exit") { text, listener ->
-                    ActivityCompat.finishAffinity(context as Activity)//closes all the instances of the app and the app closes completely
+                    ActivityCompat.finishAffinity(context as Activity)
                 }
                 alterDialog.setCancelable(false)
 
